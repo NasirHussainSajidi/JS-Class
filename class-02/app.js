19eb86a2fd515580a346a06785fd149efb8e4604
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-07-21 22:28:31 +0500

    Add files via upload

---
+++ class-02/app.js
@@ -0,0 +1,62 @@
+// Hosting
+
+// console.log("Hi ", a)
+//  var a = "Ali"
+
+// console.log("Hi !", a);
+
+
+// concatenation always apply on  + operation
+
+
+// let FirstName = "Nasir"
+// let LastName = "Hussain"
+// let FullName = FirstName + "  " + LastName
+// console.log("Hi ", FullName)
+
+
+// const FirstName = "Nasir"
+// const age = 20
+// const data = FirstName + " "+ age
+// console.log("Hi ", data)
+
+
+const n1 = "10"
+const n2 = 5
+const result = n1 - n2
+console.log("Result is ", result)
+
+console.log("10 " === 10)
+
+
+// Data Types In JAVASCRIPT
+
+// const name = "Nasir"  // String
+
+// const age = 20 // Number
+
+ 
+// const isStudent = false  // Boolean
+
+// console.log("Name is ", name, "Age is ", age, "Is Student? ", isStudent)
+
+// const data = null // Null
+// const data2 = undefined // Undefined
+// const object = { name: "Nasir", age: 20 } // Object
+// const array = [1, 2, 3, 4, 5] // Array
+
+// console.log("Data is ", data, "Data2 is ", data2, "Object is ", object, "Array is ", array)
+
+// const Fname = "Hussain"
+
+// console.log("Welcome " + Fname + " to our website")
+
+// console.log(`Welcome ${Fname} to our website`)
+
+
+let age = prompt("Enter your age:")
+if (age>= 18) {
+    console.log("You are eligible to vote") }
+    else {
+        console.log("You are not eligible to vote")
+    }
