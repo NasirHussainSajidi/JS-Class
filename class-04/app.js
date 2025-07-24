bf310a7e5be3a113bd07dabeaa222d3145b19b40
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-07-24 08:05:09 +0500

    Add files via upload

---
+++ class-04/app.js
@@ -0,0 +1,43 @@
+ //   Pre Increment  ---  Post Increment
+
+
+ let a = 10
+   ++a 
+ let b = 5
+   b++
+ console.log(a)   // 11
+ console.log(++a)  // 12
+ console.log(b)  //6
+ console.log(b++) //6
+
+
+
+ //   Pre Decrement  ---  Post Decrement
+
+ let c = 10
+ let d = 5
+ console.log(c)
+ console.log(--c)
+ console.log(c--)
+ console.log(d)
+ console.log(d--)
+ console.log(--d)
+
+
+
+let x = 10
+let f = x++
+console.log(f)
+
+let g = ++x
+console.log(g)
+console.log(f+g)
+
+
+let y = 5
+console.log(++y + y++ + --y + --y + y++ + y-- + ++y)
+
+
+
+
+
