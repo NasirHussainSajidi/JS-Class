115fefd2e77e23b79d1c4fbad481e30891b5a0fd
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-07-25 22:43:30 +0500

    Add files via upload

---
+++ class-05/app.js
@@ -0,0 +1,38 @@
+  // GREATER THAN
+
+  console.log(15 > 5);
+
+  // LESS THAN
+  console.log(5 < 15)
+
+  // GREATER THAN OR EQUAL TO
+  console.log(15 >= 15)
+
+    // LESS THAN OR EQUAL TO
+    console.log(5 <= 6)
+
+// DOUBLE EQUALS
+console.log("triple equal", 5 == "5")
+
+// NOT EQUALS
+
+console.log("Not Equal",6 != "5")
+
+const age = 18
+
+if(age >= 18){
+    console.log("You are eligible to vote")
+}
+else{
+    console.log("You are not eligible to vote")
+}
+
+
+const login = false
+
+if(!login){
+    console.log("Welcome to the site")
+}
+else{
+    console.log("Please login to continue")
+}
