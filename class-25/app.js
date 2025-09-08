fa2c2ef9ced2382848283c75f226cf4ad8d344c9
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-09-08 18:05:54 +0500

    Add files via upload

---
+++ class-25/app.js
@@ -0,0 +1,40 @@
+// getElementById
+// getElementByClassName
+// getElementByTagName
+// querySelector
+// querySelectorAll
+
+const  heading = document.getElementById("heading");
+console.log(heading);
+
+const paragraph = document.getElementsByClassName("para");
+console.log(paragraph);
+
+
+
+// getElementById
+
+//! Return Single Element
+// const heading = document.getElementById('heading')
+
+// getElementByClassName
+
+//! Return a HTMLCollection
+// const heading = document.getElementsByClassName('text-style')
+
+// getElementByTagName
+
+//! Return Html Collection 
+// const heading = document.getElementsByTagName('h1')
+
+// querySelector
+
+// const heading = document.querySelector('.text-style')
+
+//! Return first Element
+
+// querySelectorAll
+
+// const heading = document.querySelectorAll('.text-style')
+
+// Return first Element
